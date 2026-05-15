add90407304b2a5f22bc0ac7754429a37bf05156
1shm4m <ishmam.raiyan@cooper.edu>
2026-05-15 11:13:45 -0400

    Refactor computer module and improve readability
    Refactored module instantiation for clarity and added comments.

--- single_cycle_computer/computer.sv
+++ single_cycle_computer/computer.sv
@@ -1,24 +1,50 @@
 
-// Made by Fatin Hoque
+// By Fatin Hoque 
+//Edited By Ishmam Raiyan
+`include "../controller/controller.sv"
+`include "../datapath/datapath.sv"
+`include "../imem/imem.sv"
+`include "../dmem/dmem.sv"
+`include "../cpu/cpu.sv"
+
 `ifndef COMPUTER
 `define COMPUTER
 `timescale 1ns/100ps
-`include "cpu.sv"
-`include "imem.sv"
-`include "dmem.sv"
 
 module computer #(parameter n = 32)(
     input  logic           clk,
     input  logic           reset,
-    output logic [(n-1):0] dataadr,
-    output logic [(n-1):0] writedata,
+    output logic [(n-1):0] dataadr,   // Output to the outside world
+    output logic [(n-1):0] writedata, // Output to the outside world
     output logic           memwrite);
 
     logic [(n-1):0] pc, instr, readdata;
 
-    dmem dmem(clk, memwrite, dataadr, writedata, readdata);
-    imem imem(pc[7:2], instr);
-    cpu  mips(clk, reset, pc, instr, memwrite, dataadr, writedata, readdata);
+    // imem provides 'instr' based on 'pc'
+    imem imem (
+        .addr(pc[7:2]), 
+        .readdata(instr)
+    );
 
+    // dmem receives 'dataadr' and 'writedata' from the CPU
+    dmem dmem (
+        .clk(clk), 
+        .write_enable(memwrite), 
+        .addr(dataadr), 
+        .writedata(writedata), 
+        .readdata(readdata)
+    );
+
+    // cpu drives 'aluout' (which maps to dataadr), 'writedata', and 'memwrite'
+    cpu mips (
+        .clk(clk), 
+        .reset(reset), 
+        .pc(pc), 
+        .instr(instr), 
+        .memwrite(memwrite), 
+        .aluout(dataadr),    // CPU drives the address
+        .writedata(writedata), // CPU drives the data
+        .readdata(readdata)    // CPU receives data from memory
+    );
 endmodule
 `endif
